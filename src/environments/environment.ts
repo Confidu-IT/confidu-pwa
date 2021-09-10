a289ab33529ea4c185905b1ad7425016aa4ee32f
mkalden <post@matthias-kalden.de>
2021-09-10 12:18:34 +0200

    [change] firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,15 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDRiHrFLubf7mqQoykAl216RPOh4lnzGP0',
-    authDomain: 'confidu-app.firebaseapp.com',
-    databaseURL: 'https://confidu-app.firebaseio.com',
-    projectId: 'confidu-app',
-    storageBucket: 'confidu-app.appspot.com',
-    messagingSenderId: '944987547095',
-    appId: '1:944987547095:web:73f32524698d702a0f1792',
-    measurementId: 'G-XZB7VR6BZ3',
-    vapidKey: 'BMqCmorVeDKwwwQzRVEVSXU5UVnWkzZfB1kAcMr6acpoIhjJ3Q3S8c-VC_PDZtr7VUj_PW6Mev4ucRs6t50YCe4'
+    apiKey: "AIzaSyDq1kB201k3JwAQn16Cc87XUeiZkPOj4t8",
+    authDomain: "confidu-app-test.firebaseapp.com",
+    databaseURL: "https://confidu-app-test-default-rtdb.firebaseio.com",
+    projectId: "confidu-app-test",
+    storageBucket: "confidu-app-test.appspot.com",
+    messagingSenderId: "862692374801",
+    appId: "1:862692374801:web:6a0a2ec0ef4af53cfbb4b0",
+    measurementId: "G-R2NPP4R9X8"
+    // vapidKey: 'BMqCmorVeDKwwwQzRVEVSXU5UVnWkzZfB1k-cMr6acpoIhjJ3Q3S8c-VC_PDZtr7VUj_PW6Mev4ucRs6t50YCe4'
   },
   logo: '../assets/icons/logo_confid.svg',
   baseUrl: 'https://staging-backend-dot-confidu-app.ey.r.appspot.com',
